e26028d9bae3398fa492bc6386b1cc9c675fbc96
seungsura <seungsura@gmail.com>
2023-04-02 12:32:35 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Navbar from "../components/NavBar";
 import LeftBody from "../components/LeftBody";
 import RightBody from "../components/RightBody";
-import MainBody from "../components/MainBody";
 
 export default function Home() {
   return (
