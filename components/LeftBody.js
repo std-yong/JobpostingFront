b8f93f3ed97c2dec991789ab19e3ac75b7b59e7b
seungsura <seungsura@gmail.com>
2023-04-05 10:31:46 +0000

    Plus Categories

--- components/LeftBody.js
+++ components/LeftBody.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/LeftBody.module.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default LeftBody;
 
