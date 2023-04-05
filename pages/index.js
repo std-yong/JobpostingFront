36c7ce0647cd7afd161ea1977ed053c59a1cbea3
seungsura <seungsura@gmail.com>
2023-04-05 06:24:11 +0000

    backup

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
-import TestNavBar from "../components/TestNavBar"
-import MainBody from "../components/MainBody";
-import CheckBox from "../components/CheckBox";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function Home () {
+export default function Home() {
   return (
-    <>
-      <TestNavBar />
-      <CheckBox/>
-      <MainBody />
-    </>
+    <div className="d-flex 
+justify-content-center align-items-center">
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-export default Home;
