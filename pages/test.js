eef851742fd6f3220436631364fafdb37c5248f4
seungsura <seungsura@gmail.com>
2023-04-06 06:34:21 +0000

    Edit CheckBox

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,16 @@
 import TestNavBar from "../components/TestNavBar"
 import MainBody from "../components/MainBody";
-import CheckBox from "../components/CheckBox";
 import BootstrapHead from "../components/BootstrapHead";
+import FilterBox from "../components/FilterBox";
 
 function Home () {
   return (
     <>
-      <TestNavBar />
-      
-      <MainBody />
+      <TestNavBar style={{ position: 'fixed', zIndex: 1 }} />
+      <div style={{ paddingTop: '74px' }}>
+        <FilterBox />
+        <MainBody />
+      </div>
     </>
   );
 }
