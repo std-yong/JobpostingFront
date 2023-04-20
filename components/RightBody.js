4a4d7a925214b060acb1dc22b24624d93f425d98
seungsura <90497686+seungsura@users.noreply.github.com>
2023-04-20 09:58:43 +0900

    Update RightBody.js

--- components/RightBody.js
+++ components/RightBody.js
@@ -1,58 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/RightBody.module.css";
 import BootstrapHead from "./BootstrapHead";
+import { getContent } from "../pages/api";
+
+function daysRemaining(endDate) {
+  const today = new Date();
+  const end = new Date(endDate);
+  const diff = Math.ceil((end - today) / (1000 * 60 * 60 * 24));
+  return diff;
+}
 
 export default function RightBody() {
+  const [jobData, setJobData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getContent();
+      const filteredData = data.filter(
+        (job) => daysRemaining(job.end_date) > 0
+      );
+      filteredData.sort((a, b) => new Date(a.end_date) - new Date(b.end_date));
+      setJobData(filteredData.slice(0, 4));
+    };
+    fetchData();
+  }, []);
+
   return (
-    <><BootstrapHead/>
-    <div className={styles.right_body}>
-      <div className={styles.feed_name}>
-        <div className={styles.flex_center}>
-          <div className={styles.big_profile_box}>
-            <img className={styles.profile_img} src="rightbody/ProfileIU.jpg" />
+    <>
+      <BootstrapHead />
+      <div className={styles.right_body}>
+        {jobData.map((job, index) => (
+          <div key={job.id} className={styles.feed_name}>
+            <div className={styles.flex_center}>
+              <div
+                className={index === 0 ? styles.big_profile_box : styles.profile_box}
+              >
+                <img className={styles.profile_img} src={job.company_image_url} />
+              </div>
+              <div className={styles.name_content}>
+                <span className={styles.feed_name_txt}>{job.company_name}</span>
+                <span className={styles.name_content_txt}>{job.job_description}</span>
+              </div>
+            </div>
+            <a className={styles.link_txt}>D-{daysRemaining(job.end_date)}</a>
           </div>
-          <div className={styles.name_content}>
-            <span className={styles.feed_name_txt}> Company1</span>
-            <span className={styles.name_content_txt}> 고치기 귀찮다 이제는</span>
-          </div>
-        </div>
-        <a className={styles.link_txt}>지원</a>
+        ))}
       </div>
-      <div className={styles.recommend_box}>
-      </div>
-      <div>
-        <div className={styles.feed_name}>
-          <div className={styles.profile_box}>
-            <img className={styles.profile_img} src="rightbody/ProfileJjang.jpg" />
-          </div>
-          <div className={styles.name_content}>
-            <span className={styles.feed_name_txt}>Company2</span>
-            <span className={styles.name_content_txt}> 오늘은 귀찮다</span>
-          </div>
-          <a className={styles.link_txt}>지원</a>
-        </div>
-        <div className={styles.feed_name}>
-          <div className={styles.profile_box}>
-            <img className={styles.profile_img} src="rightbody/ProfileLuffy.jpg" />
-          </div>
-          <div className={styles.name_content}>
-            <span className={styles.feed_name_txt}>Company3</span>
-            <span className={styles.name_content_txt}> 돈없는 백수는 싫어</span>
-          </div>
-          <a className={styles.link_txt}>지원</a>
-        </div>
-        <div className={styles.feed_name}>
-          <div className={styles.profile_box}>
-            <img className={styles.profile_img} src="rightbody/ProfileSponge.webp" />
-          </div>
-          <div className={styles.name_content}>
-            <span className={styles.feed_name_txt}> Company4</span>
-            <span className={styles.name_content_txt}> 헬기업</span>
-          </div>
-          <a className={styles.link_txt}>지원</a>
-        </div>
-      </div>
-    </div>
     </>
   );
 }
